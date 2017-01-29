ef8ba0fea59f18a917e42074a2ea0d384063aa40
Patrick Ford <patrickford@msn.com>
2017-01-29 13:48:38 -0800

    fixes

--- hashmap.js
+++ hashmap.js
@@ -1,59 +1,4 @@
-var HashMap = function(initialCapacity) {
-    this.length = 0;
-    this._slots = [];
-    this._capacity = initialCapacity || 8;
-};
-HashMap.MAX_LOAD_RATIO = 0.9;
-HashMap.SIZE_RATIO = 3;
-
-HashMap._hashString = function(string) {
-    var hash = 5381;
-    for (var i=0; i<string.length; i++) {
-        hash = (hash << 5) + hash + string.charCodeAt(i);
-        hash = hash & hash;
-    }
-    return hash >>> 0;
-};
-
-HashMap.prototype.set = function(key, value) {
-    var loadRatio = (this.length + 1) / this._capacity;
-    if (loadRatio > HashMap.MAX_LOAD_RATIO) {
-        this._resize(this._capacity * HashMap.SIZE_RATIO);
-    }
-
-    var index = this._findSlot(key);
-    this._slots[index] = {
-        key: key,
-        value: value
-    };
-    this.length++;
-};
-
-HashMap.prototype._findSlot = function(key) {
-    var hash = HashMap._hashString(key);
-    var start = hash % this._capacity;
-
-    for (var i=start; i<start + this._capacity; i++) {
-        var index = i % this._capacity;
-        var slot = this._slots[index];
-        if (slot === undefined || slot.key == key) {
-            return index;
-        }
-    }
-    // Unreachable
-};
-
-HashMap.prototype._resize = function(size) {
-    var oldSlots = this._slots;
-    this._capacity = size;
-    this._slots = [];
-    for (var i=0; i<oldSlots.length; i++) {
-        var slot = oldSlots[i];
-        if (slot !== undefined) {
-            this.set(slot.key, slot.value);
-        }
-    }
-};
+"use strict";
 
 var HashMap = function(initialCapacity) {
     this.length = 0;
@@ -64,12 +9,13 @@ var HashMap = function(initialCapacity) {
 HashMap.MAX_LOAD_RATIO = 0.9;
 HashMap.SIZE_RATIO = 3;
 
-HashMap.prototype.get = function(key) {
-    var index = this._findSlot(key);
-    if (this._slots[index] === undefined) {
-        throw new Error('Key error');
+HashMap.prototype._hashString = function(string) {
+    var hash = 5381;
+    for (var i=0; i<string.length; i++) {
+        hash = (hash << 5) + hash + string.charCodeAt(i);
+        hash = hash & hash;
     }
-    return this._slots[index].value;
+    return hash >>> 0;
 };
 
 HashMap.prototype.set = function(key, value) {
@@ -87,6 +33,29 @@ HashMap.prototype.set = function(key, value) {
     this.length++;
 };
 
+HashMap.prototype._findSlot = function(key) {
+    var hash = this._hashString(key);
+
+    var start = hash % this._capacity;
+    console.log(start)
+    for (var i=start; i<start + this._capacity; i++) {
+        var index = i % this._capacity;
+        var slot = this._slots[index];
+        if (slot === undefined || (slot.key == key && !slot.deleted)) {
+            return index;
+        }
+    }
+    // Unreachable
+};
+
+HashMap.prototype.get = function(key) {
+    var index = this._findSlot(key);
+    if (this._slots[index] === undefined) {
+        throw new Error('Key error');
+    }
+    return this._slots[index].value;
+};
+
 HashMap.prototype.remove = function(key) {
     var index = this._findSlot(key);
     var slot = this._slots[index];
@@ -96,20 +65,6 @@ HashMap.prototype.remove = function(key) {
     slot.deleted = true;
     this.length--;
     this._deleted++;
-};
-
-HashMap.prototype._findSlot = function(key) {
-    var hash = HashMap._hashString(key);
-    var start = hash % this._capacity;
-
-    for (var i=start; i<start + this._capacity; i++) {
-        var index = i % this._capacity;
-        var slot = this._slots[index];
-        if (slot === undefined || (slot.key == key && !slot.deleted)) {
-            return index;
-        }
-    }
-    // Unreachable
 };
 
 HashMap.prototype._resize = function(size) {
